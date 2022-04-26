870293d6b8fa3a4f457a5e0a2c1780fec4eeaf93
KBulu <55754565+BuluCoder@users.noreply.github.com>
2022-04-26 14:38:02 +0100

    spawning of objects

--- GE2 Test 2022 Starter Unity/Assets/Nematode.cs
+++ GE2 Test 2022 Starter Unity/Assets/Nematode.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class Nematode : MonoBehaviour
 {
-    public int length = 5;
+    public int length;
 
     public Material material;
 
     void Awake()
     {
-        // Put your code here!
+        for (int i = 0; i < length; i++)
+        {
+            GameObject nema = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            nema.transform.position = new Vector3(0, 0, -i);
+
+            float width = 1.5f - (i / (float)length);
+            float height = 1.5f - (i / (float)length);
+
+            nema.transform.localScale = new Vector3(width, height, 1);
+            nema.transform.parent = this.transform;
+
+            nema.GetComponent<Renderer>().material.color = Color.HSVToRGB(i / (float)length, 1, 1);
+        }
     }
 
 
